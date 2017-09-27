8940d74555b529b2f309f2136221303612ed8aeb
e <2312118379@qq.com>
2017-09-27 15:41:37 +0800

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,23 @@
+//#include<iostream> 
+//using namespace std;
+//int main()
+//{
+//	int H=411;
+//	int n=200;
+//	int **path=new int*[H];
+//	for (int i=0;i<H;i++){
+//		path[i]=new int[n];
+//	}
+//	int k=10;
+//	for (int i=0;i<H;i++)
+//	{
+//		for(int j=0;j<n;j++)
+//		{
+//	        path[k][j]=i+j;
+//			cout<<path[i][j]<<"  ";
+//		}
+//		cout<<endl;
+//	}
+//	system("pause");
+//
+//}
